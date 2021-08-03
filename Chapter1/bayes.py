1fb8acfc85ab930b0aedc95c85b056b5251619bd
zkkamir <zkkamir@gmail.com>
2021-08-03 08:56:16 +0430

    Installed dependences and started chapter 1.

---
+++ Chapter1/bayes.py
@@ -0,0 +1,11 @@
+import sys
+import random
+import itertools
+import numpy as np
+import cv2 as cv
+
+MAP_FILE = "cape_python.png"
+
+SA1_CORNERS = (130, 265, 180, 315) # (UL-X, UL-Y, LR-X, LR-Y)
+SA2_CORNERS = (80, 255, 130, 305) # (UL-X, UL-Y, LR-X, LR-Y)
+SA3_CORNERS = (105, 205, 155, 255) # (UL-X, UL-Y, LR-X, LR-Y)
